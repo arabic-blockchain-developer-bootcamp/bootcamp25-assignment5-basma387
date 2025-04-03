d959cae2886a1fd5e394e32a5eb507decca0b4ec
basma387 <basmayasser022@gmail.com>
2025-04-03 22:50:44 +0200

    Update Assignment5.sol

--- src/Assignment5.sol
+++ src/Assignment5.sol
@@ -3,24 +3,24 @@ pragma solidity ^0.8.0;
 
 contract Assignment5 {
     // 1. Declare a public unsigned integer variable called `counter`
-uint counter;
+    uint public counter;
     // 2. Declare a public boolean variable called `isActive`
-bool isActive;
+    bool public isActive;
     // 3. Create a public mapping from an address to a string called `userNames`
-mapping(address=>string)public userNames;
+    mapping(address => string) public userNames;
     // 4. Define a struct `User` with fields: `id` (uint), `name` (string)
-struct User {
-uint id;
-string name;
-}
+    struct User {
+        uint id;
+        string name;
+    }
     // 5. Create a public array of `User` called `users`
-User [] public users;
+    User[] public users;
+    
     // Constructor
     // Initialize `isActive` to true
     constructor() {
         // Fill in the logic
-isActive = true;
-
+        isActive = true;
     }
 
     // Function to increment the counter
@@ -29,9 +29,9 @@ isActive = true;
     // - Use a for loop to increment `counter` by 1 for 5 iterations
     function incrementCounter() external {
         // Fill in the logic
-      for (uint i=0;i<5;i++){
-counter++
-}
+        for (uint i = 0; i < 5; i++) {
+            counter++;
+        }
     }
 
     // Function to toggle `isActive`
@@ -40,25 +40,25 @@ counter++
     // - Use an if statement to toggle `isActive` between true and false
     function toggleActive() public {
         // Fill in the logic
-        if (isActive){
-isActive=fales;}
-else{ isActive=true;
-}
+        if (isActive) {
+            isActive = false;
+        } else {
+            isActive = true;
+        }
     }
 
     // Function to add a user
     // This function should:
     // - Be public
     // - Take three parameters: `id` (uint) and `name` (string) and userAddress (address)
-        // Hint: Define string as `string memory`
-        // The memory keyword will be explained later
+    // Hint: Define string as `string memory`
+    // The memory keyword will be explained later
     // - Add a new user to the `users` array
     // - Update the `userNames` mapping with the address and name
     function addUser(uint _id, string memory _name, address _userAddress) public {
         User memory newUser = User(_id, _name);
         users.push(newUser);
-        userNames[_userAddress]= -name;
-
+        userNames[_userAddress] = _name;
     }
 
     // Function to get the total number of users
@@ -67,6 +67,5 @@ else{ isActive=true;
     // - Return the length of the `users` array
     function getUserCount() public view returns (uint) {
         return users.length;
-
     }
 }
